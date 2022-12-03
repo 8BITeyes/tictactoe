1aa3f0ede6b91c20148210eff8e4565656991a13
RyanFrazin <RyanFrazin@Gmail.com>
2022-12-02 19:55:47 -0800

    Ryan Frazin

--- script.js
+++ script.js
@@ -3,36 +3,62 @@
 const gameBoard = (() => {
     let wholeBoard = document.querySelector('#gameboard'); //full game board container
     
-    let display = [];       //9 sections display of tic tac toe board
+    let displayGame = [];       //9 sections display of tic tac toe board
     for(let i = 0; i < 9; i++) {
-        display.push('');
-    };
-    display.forEach(item => {
+        displayGame.push('');
         let piece = document.createElement('div');
-        piece.className = "square";
+        piece.className = `square-${[i]}`;
         wholeBoard.appendChild(piece);
-    });
+    }
 
-    let game = ['x','o','x','o','x','o','x','o','x'];
-    function changeDisplay() {     //changes text of display sections to associated game content (i.e."x" or "o")
+    function changeDisplay() {     //loops through display array to change text content of item in array
         for(let i = 0; i < 9; i++) {
-            display[i] = game[i];
-            let x = document.querySelector(`#gameboard > div:nth-child(${i + 1})`);
-            x.textContent = display[i];
+            let square = document.querySelector(`#gameboard > div:nth-child(${i + 1})`);
+            displayGame[i] = square.textContent;
         }
-    };
-
-    return {game, changeDisplay}
+    }
+    return {displayGame, changeDisplay};
 })();
 
 //GAME OBJECT
 
-/*const gameFlow = (() => {
+const gameFlow = (() => {       //determines whose turn on the board it is; Player One: 'x' or Player Two: 'o'
+    let sections = document.querySelectorAll('#gameboard > div');
+    for (const section of sections) {
+        section.addEventListener('click', function onClick() {
+            if (section.textContent === '') {
+                if (getOccurance() === 'x') {
+                    section.textContent = `${playerTwo.type}`;
+                } else if (getOccurance() === 'o') {
+                    section.textContent = `${playerOne.type}`;
+                } else if(getOccurance() === 'tied'){
+                    section.textContent = `${playerOne.type}`;
+                }
+            }
+            gameBoard.changeDisplay();
+        });
+    };
+    function getOccurance() {       //Determines whether 'x' or 'o' is dominated in displayGame array in gameBoard
+        let x = 0;
+        let o = 0;
+        gameBoard.displayGame.forEach((item) => (item === 'x' && x++));
+        gameBoard.displayGame.forEach((item) => (item === 'o' && o++));
 
-})();*/
+        if(x > o) {
+            return 'x';
+        } else if (o > x) {
+            return 'o';
+        } else {
+            return 'tied';
+        }
+    }
+})();
 
 //PLAYER FACTORY
 
 let Player = (type, name) => {
     return {type, name};
 };
+
+const playerOne = Player('x', 'Jeff');
+const playerTwo = Player('o', 'Jeffica');
