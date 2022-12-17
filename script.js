368dcdff74b5252196c19167ba4f9bc9544f32b4
RyanFrazin <RyanFrazin@Gmail.com>
2022-12-16 17:14:29 -0800

    Ryan Frazin Dec 16, 2022; 17:14
    Add function checkWinner() that checks if a player has won by checking total number of move
    combinations necessary to win game.
    
    Add total possible combinations needed to win game. (winningCombo)

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
     let sections = document.querySelectorAll('#gameboard > div');
     for (const section of sections) {
         section.addEventListener('click', function onClick() {
-            if (section.textContent === '') {
+            if (section.textContent === '') {       //only changes board section content if blank
                 if (getOccurance() === 'x') {
                     section.textContent = `${playerTwo.type}`;
                 } else if (getOccurance() === 'o') {
@@ -38,6 +38,7 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
             gameBoard.changeDisplay();
         });
     };
+    
     function getOccurance() {       //Determines whether 'x' or 'o' is dominated in displayGame array in gameBoard
         let x = 0;
         let o = 0;
@@ -51,7 +52,75 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
         } else {
             return 'tied';
         }
-    }
+    };
+    
+    const winningCombo = [
+        [0, 1, 2],
+        [0, 4, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [2, 4, 6],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
+
+    const playerOneIndex = [];
+    const playerTwoIndex = [];
+    for (const section of sections) {           //checks the board if either player has won the game
+        section.addEventListener('click', function checkWin() {
+            for (let i = 0; i < gameBoard.displayGame.length; i++) { //gameBoard.displayGame in first IIFE
+                if(gameBoard.displayGame[i] === 'x') {
+                    switch(playerOneIndex.includes(i)) {
+                        case false:
+                            playerOneIndex.push(i);
+                            break;
+                        case true:
+                            break;
+                    }
+                }
+                else if(gameBoard.displayGame[i] === 'o') {
+                    switch(playerTwoIndex.includes(i)) {
+                        case false:
+                            playerTwoIndex.push(i);
+                            break;
+                        case true:
+                            break;
+                    }
+                }
+            };
+            console.log(checkWinner());
+        });
+    };
+
+    const checkWinner = () => {
+        let winningPlayer;
+        winningCombo.forEach((key) => {
+            switch(key.every(key => playerOneIndex.includes(key))) {
+                case false:
+                    break;
+                case true:
+                    if(winningPlayer === undefined) { //If winningPlayer is already defined, then switch breaks
+                        winningPlayer = playerOne.name;
+                    }else {
+                        break;
+                    }
+            };
+            switch(key.every(key => playerTwoIndex.includes(key))) {
+                case false:
+                    break;
+                case true:
+                    if(winningPlayer === undefined) {
+                        winningPlayer = playerTwo.name;
+                    } else {
+                        break;
+                    }
+            };
+        });
+        return winningPlayer;
+    };
+
+    return {playerOneIndex, playerTwoIndex, checkWinner}
 })();
 
 //PLAYER FACTORY
