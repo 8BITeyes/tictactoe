ab88f61544bf0a206c47f838646c7d5452f494c6
RyanFrazin <RyanFrazin@Gmail.com>
2022-12-21 10:02:09 -0800

    Ryan Frazin Dec 21, 2022; 10:02
    Add functionality to form and submission of form
    
    Form will now change player names and display of players on game board
    
    Add functionality to board once form is submitted
    
    Add number of wins and 'change name' buttons to player display
    
    Define form and buttons in gameBoard IIFE now

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ const gameBoard = (() => {
         }
     }
 
-    return {displayGame, changeDisplay};
+    let form = document.getElementById('player-form');
+    let changeNames = document.querySelectorAll('.change');
+
+    return {displayGame, changeDisplay, wholeBoard, changeNames, form};
 })();
 
 //GAME OBJECT
@@ -30,11 +33,11 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
         section.addEventListener('click', function onClick() {
             if (section.textContent === '') {       //only changes board section content if blank
                 if (getOccurance() === 'x') {
-                    section.textContent = `${playerTwo.type}`;
+                    section.textContent = `${playerSubmit.playerTwo.type}`; //PlayerOne and Two type below after form submit
                 } else if (getOccurance() === 'o') {
-                    section.textContent = `${playerOne.type}`;
+                    section.textContent = `${playerSubmit.playerOne.type}`;
                 } else if(getOccurance() === 'tied'){
-                    section.textContent = `${playerOne.type}`;
+                    section.textContent = `${playerSubmit.playerOne.type}`;
                 }
             }
             gameBoard.changeDisplay();
@@ -44,8 +47,8 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
     function getOccurance() {       //Determines whether 'x' or 'o' is dominated in displayGame array in gameBoard
         let x = 0;
         let o = 0;
-        gameBoard.displayGame.forEach((item) => (item === 'x' && x++));
-        gameBoard.displayGame.forEach((item) => (item === 'o' && o++));
+        gameBoard.displayGame.forEach((item) => (item === 'X' && x++));
+        gameBoard.displayGame.forEach((item) => (item === 'O' && o++));
 
         if(x > o) {
             return 'x';
@@ -72,7 +75,7 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
     for (const section of sections) {           //checks the board if either player has won the game
         section.addEventListener('click', function checkWin() {
             for (let i = 0; i < gameBoard.displayGame.length; i++) { //gameBoard.displayGame in first IIFE
-                if(gameBoard.displayGame[i] === 'x') {
+                if(gameBoard.displayGame[i] === 'X') {
                     switch(playerOneIndex.includes(i)) {
                         case false:
                             playerOneIndex.push(i);
@@ -81,7 +84,7 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
                             break;
                     }
                 }
-                else if(gameBoard.displayGame[i] === 'o') {
+                else if(gameBoard.displayGame[i] === 'O') {
                     switch(playerTwoIndex.includes(i)) {
                         case false:
                             playerTwoIndex.push(i);
@@ -115,7 +118,7 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
                     break;
                 case true:
                     if(winningPlayer === undefined) { //If winningPlayer is already defined, then switch breaks
-                        winningPlayer = playerOne.name;
+                        winningPlayer = playerSubmit.playerOne.name;
                         displayWinner.textContent = `${winningPlayer} wins`;  //Adds name of winning player visually to page
                         break;
                     }
@@ -125,7 +128,7 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
                     break;
                 case true:
                     if(winningPlayer === undefined) {
-                        winningPlayer = playerTwo.name;
+                        winningPlayer = playerSubmit.playerTwo.name;
                         displayWinner.textContent = `${winningPlayer} wins`;
                     } else {
                         break;
@@ -137,14 +140,45 @@ const gameFlow = (() => {       //determines whose turn on the board it is; Play
 
     const displayWinner = document.querySelector('#winner'); //displays winner on page using #winner div
 
-    return {playerOneIndex, playerTwoIndex, checkWinner}
+    return {playerOneIndex, playerTwoIndex, checkWinner, getOccurance}
+})();
+
+
+//PLAYER SUBMIT FORM
+
+const playerSubmit = (() => {
+    let Player = (type, name) => {
+        return {type, name};
+    };
+
+    let firstPlayerInput = document.getElementById('player-one'); //Form input of player names
+    let secondPlayerInput = document.getElementById('player-two');
+
+    let playerOne = Player('X', firstPlayerInput.value);
+    let playerTwo = Player('O', secondPlayerInput.value);
+
+    let firstPlayerName = document.querySelector('.player:first-child > .name'); //Display of player names on page
+    let secondPlayerName = document.querySelector('.player:nth-child(2) > .name');
+
+
+    const submitButton = document.querySelector('#confirm'); //Form submit button
+    submitButton.addEventListener('click', function addPlayers() {
+        if(firstPlayerInput.value === '' || secondPlayerInput.value === '') {
+            alert('please fill in all player names');
+        } else {
+            playerSubmit.playerOne = Player('X', firstPlayerInput.value); //'This' changes so must use playerSubmit.playerOne
+            playerSubmit.playerTwo = Player('O', secondPlayerInput.value);
+    
+            firstPlayerName.textContent = playerSubmit.playerOne.name;
+            secondPlayerName.textContent = playerSubmit.playerTwo.name;
+
+            gameBoard.form.style.display = 'none';
+            gameBoard.changeNames.forEach(button => button.style.pointerEvents = "auto");
+            gameBoard.wholeBoard.style.pointerEvents = "auto";
+        }
+    });
+
+    return{playerOne, playerTwo};
 })();
 
 //PLAYER FACTORY
-
-let Player = (type, name) => {
-    return {type, name};
-};
-
-const playerOne = Player('x', 'Jeff');
-const playerTwo = Player('o', 'Jeffica');
